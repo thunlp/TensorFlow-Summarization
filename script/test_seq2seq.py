92863feee186dc08ac5ab92cd5910d76edae3d52
Lei Xu <leonard.xu.thu@gmail.com>
2017-02-24 15:18:19 +0800

    training scripts.

---
+++ script/test_seq2seq.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import subprocess
+import logging
+import os
+
+model_path = "model_seq/"
+model_pattern = "model_seq/{}.ckpt-{}"
+data_pattern = "data/test.{}.txt"
+OUTPUT_DIR = "beam_seq/"
+OUTPUT_PATTERN = OUTPUT_DIR + "{dataset}.{description}.txt"
+MAX_KEEP = 1
+
+datasets = ["giga", "duc2003", "duc2004"]
+beam_searchs = [1]
+
+test_params = {
+    "--decode": True,
+    "--fast_decode": True
+}
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
+                        datefmt='%b %d %H:%M')
+
+    try:
+        os.mkdir(OUTPUT_DIR)
+    except:
+        pass
+
+    ckpts = os.listdir(model_path)
+    models = []
+    for item in ckpts:
+        toks = item.split('.')
+        if len(toks) != 3:
+            continue
+        if toks[2] != "index":
+            continue
+        toks[1] = toks[1].split('-')[1]
+        models.append(toks[:2])
+    models = sorted(models, key = lambda x: int(x[1]), reverse=True)
+    models = models[:MAX_KEEP]
+    print(models)
+
+    for model in models:
+        ckpt = model_pattern.format(model[0], model[1])
+        logging.info("Test {}. ".format(ckpt))
+        for dataset in datasets:
+            for beam_search in beam_searchs:
+                logging.info("Test {} with beam_size = {}".format(data_pattern.format(dataset), beam_search))
+                output_file = OUTPUT_PATTERN.format(dataset=dataset, description=str(beam_search)+"_"+str(model[1]))
+                if os.path.exists(output_file):
+                    logging.info("{} exists, skip testing".format(output_file))
+                    continue
+                proc = ["python3", "src/seq2seq.py", "--test_file", data_pattern.format(dataset), \
+                        "--batch_size", str(beam_search), "--test_output", output_file, "--checkpoint", ckpt]
+                for k, v in test_params.items():
+                    proc.append(k)
+                    proc.append(str(v))
+
+                subprocess.call(proc)
---
+++ script/train_seq2seq.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import subprocess
+import logging
+import os
+
+MAX_STEPS = 300000
+STEPS_PER_VALIDATION = 1000
+STEPS_PER_CHECKPOINT = 20000
+TEST_THRESHOLD = 200000
+
+train_params = {
+    "--steps_per_validation": STEPS_PER_VALIDATION,
+    "--steps_per_checkpoint": STEPS_PER_CHECKPOINT,
+}
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
+                        datefmt='%b %d %H:%M')
+
+    try:
+        global_step = tf.contrib.framework.load_variable("model_seq", "global_step")
+    except:
+        global_step = 0
+
+    logging.info("Training starts with global_step={}. ".format(global_step))
+
+    while global_step < MAX_STEPS:
+        terminate_step = max(global_step + STEPS_PER_CHECKPOINT, TEST_THRESHOLD)
+
+        logging.info("Train from {} to {}. ".format(global_step, terminate_step))
+
+        proc = ["python3", "src/seq2seq.py", "--max_iter", str(terminate_step)]
+        for key, val in train_params.items():
+            proc.append(key)
+            proc.append(str(val))
+        subprocess.call(proc)
+
+        global_step = terminate_step
+
+        subprocess.call(["python3", "script/test_seq2seq.py"])
